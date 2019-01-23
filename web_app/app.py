2929ae33d535c8477979a36f73bec4fd92b6bf1e
Simon Torres <storres@ctio.noao.edu>
2019-01-23 18:15:45 -0300

    Added experiement using socketio and flask for asynchronus data update

--- web_app/app.py
+++ web_app/app.py
@@ -1,12 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, copy_current_request_context
+from flask_socketio import SocketIO, emit
+from random import random
+from time import sleep
+from threading import Thread, Event
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+socketio = SocketIO(app)
+
+#random number Generator Thread
+thread = Thread()
+thread_stop_event = Event()
+
+class RandomThread(Thread):
+    def __init__(self):
+        self.delay = 10
+        super(RandomThread, self).__init__()
+
+    def randomNumberGenerator(self):
+        """
+        Generate a random number every 1 second and emit to a socketio instance (broadcast)
+        Ideally to be run in a separate thread?
+        """
+        #infinite loop of magical random numbers
+        print("Making random numbers")
+        while not thread_stop_event.isSet():
+            number = round(random()*10, 3)
+            print(number)
+            socketio.emit('newnumber', {'number': number}, namespace='/test')
+            sleep(self.delay)
+
+    def run(self):
+        self.randomNumberGenerator()
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    # need visibility of the global thread object
+    global thread
+    print('Client connected')
+
+    #Start the random number generator thread only if the thread has not been started before.
+    if not thread.isAlive():
+        print("Starting Thread")
+        thread = RandomThread()
+        thread.start()
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+# @socketio.on('my event')
+# def test_message(message):
+#     emit('my response', {'data': 'got it!'})
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    socketio.run(app, host='0.0.0.0', port=4000)
+    # app.run(debug=True, host='0.0.0.0', port=4000)
