92fbbf3cb59fedfd51ef0c1f3675d42f6f80fb27
Simon Torres <storres@ctio.noao.edu>
2019-01-29 16:51:49 -0300

    limited python version to 3.6 in order to get celery working with rpc:// backend added ccdproc for printing some values from header (just experimenting)

--- file_watch/worker_app.py
+++ file_watch/worker_app.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 
 app = Celery('worker',
+             backend=os.environ['CELERY_BACKEND_URL'],
              broker=os.environ['CELERY_BROKER_URL'],
              include='worker_tasks')
 
--- file_watch/worker_tasks.py
+++ file_watch/worker_tasks.py
@@ -1,7 +1,20 @@
 from __future__ import absolute_import
 
+import os
+from ccdproc import CCDData
+
 from worker_app import app
 
 @app.task
 def reduce(filename):
+    if os.path.isfile(filename):
+        ccd = CCDData.read(filename, unit='adu')
+        info = "\nOBJECT: {:s}\n" \
+               "OBSTYPE: {:s}\n" \
+               "RA: {:s} \n" \
+               "DEC: {:s}".format(ccd.header['OBJECT'],
+                                  ccd.header['OBSTYPE'],
+                                  ccd.header['RA'],
+                                  ccd.header['DEC'])
+        print(info)
     return "Reducing {}".format(filename)
