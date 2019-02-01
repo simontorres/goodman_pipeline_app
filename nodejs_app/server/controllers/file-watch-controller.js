2f2df5f471314d507da7f4c75e91214af1df631d
Simon Torres <storres@ctio.noao.edu>
2019-02-01 00:24:02 -0300

    added server side file-watch-controller, configuration needs to be made accessible in a different way

---
+++ nodejs_app/server/controllers/file-watch-controller.js
@@ -0,0 +1,37 @@
+RabbitMQConsumer = require('../rabbitmq-consumer'); //specify the name of the file here containing the code we just wrote
+
+var configuration = {
+    userName: "user",
+    password: "pass",
+    host: "rabbit",
+    port: 5672,
+    virtualHost: "rvhost",
+    exchangeName: "exchange",
+    exchangeType: "fanout",
+    exchangeOptions: {
+        durable: false,
+        internal: false,
+        autoDelete: true
+    },
+    queueName: "nodeapp",
+    queueOptions: {
+        exclusive: true,
+        durable: true,
+        autoDelete: false
+    },
+    routingKey: "routingkey"
+};
+
+
+module.exports.watch = function(req, res) {
+    var consumer = new RabbitMQConsumer(configuration);
+    consumer.on('received', function(messageText) {
+        console.log('Message received:', messageText);
+    });
+
+    consumer.consume();
+}
+
+module.exports.list = function (req, res) {
+    res.json({'name': 'simon'})
+}
