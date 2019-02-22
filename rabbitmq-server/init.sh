cc991cb213c2f3e8094d98ec00ac64732fdf5b26
Simon Torres <storres@ctio.noao.edu>
2019-02-22 15:38:43 -0300

    manual user settings for rabbitmq server

--- rabbitmq-server/init.sh
+++ rabbitmq-server/init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-
+rabbitmqctl delete_user guest
+rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS
+rabbitmqctl add_vhost $RABBITMQ_DEFAULT_VHOST
 rabbitmqctl set_permissions -p $RABBITMQ_DEFAULT_VHOST $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
 rabbitmq-server
