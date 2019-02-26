e97a7b30f5db58715f62321f905ef7487b33a594
Simon Torres <storres@ctio.noao.edu>
2019-02-26 17:16:51 -0300

    Added more data from the header to be submited to database

--- file_watch/worker_tasks.py
+++ file_watch/worker_tasks.py
@@ -13,10 +13,22 @@ def reduce(filename):
         ccd = CCDData.read(filename, unit='adu')
         raw_data = {}
         raw_data["file_name"] = os.path.basename(filename)
+        raw_data["date"] = ccd.header['DATE']
+        raw_data["ut_time"] = ccd.header['UT']
+        raw_data["instrument"] = ccd.header['INSTRUME']
+        raw_data["camera"] = ccd.header['INSTCONF']
         raw_data["object"] = ccd.header['OBJECT']
         raw_data["obstype"] = ccd.header['OBSTYPE']
         raw_data["ra"] = ccd.header['RA']
         raw_data["dec"] = ccd.header['DEC']
+        raw_data['airmass'] = ccd.header['AIRMASS']
+        raw_data['seeing'] = ccd.header['SEEING']
+        raw_data['filter'] = ccd.header['FILTER']
+        raw_data['filter2'] = ccd.header['FILTER2']
+        raw_data['grating'] = ccd.header['GRATING']
+        raw_data['slit'] = ccd.header['SLIT']
+        raw_data['wavmode'] = ccd.header['WAVMODE']
+        raw_data['exptime'] = ccd.header['EXPTIME']
 
         json_data = json.dumps(raw_data)
 
