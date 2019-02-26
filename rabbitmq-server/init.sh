3972e30d5093e69167d1e3442120b3f44029297c
Simon Torres <storres@ctio.noao.edu>
2019-02-26 18:59:40 -0300

    rabbit service now is built directly from base image

--- rabbitmq-server/init.sh
+++ rabbitmq-server/init.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+rabbitmq-server &
+sleep 10
 rabbitmqctl delete_user guest
 rabbitmqctl add_user $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS
 rabbitmqctl add_vhost $RABBITMQ_DEFAULT_VHOST
 rabbitmqctl set_permissions -p $RABBITMQ_DEFAULT_VHOST $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
-rabbitmq-server
+echo "finished"
+fg
+#rabbitmqctl stop
+#rabbitmq-server
