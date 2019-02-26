ff6dd5e01254b04111e44ba25acf912233922f5b
Simon Torres <storres@ctio.noao.edu>
2019-02-26 17:18:18 -0300

    Created API for webapp

---
+++ web/api/controllers/api_controller.js
@@ -0,0 +1,23 @@
+var request = require('request');
+
+module.exports.get_raw_files = function(req, res) {
+    request.get({url: 'http://api:8080/api/files', json:true}, function (error, response, data)  {
+        if (error) {
+            throw error;
+        }
+
+        res.json(data);
+
+    });
+};
+
+
+module.exports.put_raw_files = function (req, res) {
+    res.sendStatus(200);
+};
+
+
+module.exports.send_update_signal = function (req, res) {
+    // io.emit('messages', 'Hello from server');
+    res.sendStatus(200)
+};
--- web/webapp.js
+++ web/webapp.js
@@ -3,7 +3,9 @@
 var express          = require('express'),
     app              = express(),
     bodyParser       = require('body-parser'),
-    mongoose         = require('mongoose');
+    api_controller   = require('./api/controllers/api_controller'),
+    server           = require('http').createServer(app),
+    io               = require('socket.io')(server);
 
 
 
@@ -20,9 +22,16 @@ app.get('/', (req, res) => {
 
 app.use('/js', express.static(__dirname + '/client/js'));
 
+// API routes
 
-app.listen(PORT, HOST);
-console.log(`running on http://${HOST}:${PORT}`);
+app.get('/api/rawdata', api_controller.get_raw_files);
+app.put('/api/rawdata', api_controller.put_raw_files);
+
+app.post('/api/update', api_controller.send_update_signal);
+
+
+server.listen(PORT, HOST);
+console.log(`running webapp on http://${HOST}:${PORT}`);
 
 
 
