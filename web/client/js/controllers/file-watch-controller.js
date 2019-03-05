84bfcd76b156243f1f3933fda6b0fbf9a051dc37
Simon Torres <storres@ctio.noao.edu>
2019-03-05 14:27:34 -0300

    Added feature to follow latest files

--- web/client/js/controllers/file-watch-controller.js
+++ web/client/js/controllers/file-watch-controller.js
@@ -1,11 +1,19 @@
 app.controller('fileWatchController', ['$scope', '$resource', '$interval', function ($scope, $resource, $interval) {
+    $scope.currentPageNumber = 1;
+    $scope.currentPage = [];
+    $scope.itemsPerPage = 14;
+    $scope.follow = true;
+
     $scope.update_data = function () {
         var Files = $resource('/api/files');
 
         Files.query(function (results) {
             if (results.length > 0) {
-                $scope.files = results.reverse();
-                if ($scope.selected_file == null) {
+                $scope.files = results.slice().reverse();
+                $scope.create_pages();
+                $scope.select_page($scope.currentPageNumber);
+
+                if ($scope.selected_file == null || $scope.follow) {
                     $scope.selected_file = $scope.files[0];
                 }
             } else {
@@ -16,57 +24,62 @@ app.controller('fileWatchController', ['$scope', '$resource', '$interval', funct
             }
 
         });
-    };
-    $scope.update_data();
-    $interval($scope.update_data, 3000);
 
-    $scope.select_file = function (file) {
-        $scope.selected_file = file;
+    };
+
+    $scope.create_pages = function () {
+        // $scope.files = $scope.update_data();
+        if ($scope.files != null) {
+            $scope.totalPages = Math.ceil($scope.files.length / $scope.itemsPerPage);
+            $scope.allPagesNumbers = [];
+            $scope.allPages = [];
+            var i;
+            var startPage = 0;
+            var endPage = startPage + $scope.itemsPerPage;
+            for (i = 1; i <= $scope.totalPages; i++) {
+                $scope.allPagesNumbers.push(i);
+                $scope.allPages.push($scope.files.slice(startPage, endPage));
+                startPage = endPage;
+                endPage = startPage + $scope.itemsPerPage;
+
+
+            }
+        } else {
+            console.log('files does not exist yet!');
+        }
+
+    };
+
+
+     $scope.select_file = function (file) {
+         $scope.follow = false;
+         $scope.selected_file = file;
     };
 
     $scope.is_file_selected = function (file) {
         return $scope.selected_file._id === file._id;
     };
 
-}]);
-
-app.controller('fileListPaginationController', function($scope) {
-    $scope.currentPageNumber = 1;
-    $scope.currentPage = [];
-    $scope.itemsPerPage = 14;
-    $scope.totalPages = Math.ceil($scope.files.length / $scope.itemsPerPage);
-    $scope.allPagesNumbers = [];
-    $scope.allPages = [];
-
-    $scope.create_pages = function () {
-        var i;
-        var startPage = 0;
-        var endPage = startPage + $scope.itemsPerPage;
-        for (i = 1; i <= $scope.totalPages; i++) {
-            $scope.allPagesNumbers.push(i);
-            $scope.allPages.push($scope.files.slice(startPage, endPage));
-            startPage = endPage;
-            endPage = startPage + $scope.itemsPerPage;
-
-
-        }
-    };
-
     $scope.select_page = function (page) {
         $scope.currentPageNumber = page;
-        $scope.currentPage = $scope.allPages[page - 1];
+        if ($scope.allPages != null) {
+            $scope.currentPage = $scope.allPages[page - 1];
+        }
     };
 
     $scope.is_page_active = function (page) {
         return $scope.currentPageNumber === page;
     };
 
-    $scope.create_pages();
-    $scope.select_page($scope.currentPageNumber);
+    $scope.activate_follow = function() {
+        $scope.follow = !$scope.follow;
+    }
+
+    // startup execution
+
+    // $scope.create_pages();
 
 
+    $interval($scope.update_data, 3000);
 
-
-
-
-});
+}]);
